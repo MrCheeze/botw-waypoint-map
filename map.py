4047c64bd921f270121b4e8dbbbfba0c70f527ca
MrCheeze <fishycheeze@yahoo.ca>
2017-04-04 00:23:25 -0400

    major accuracy improvement, removed irrelevant dots

--- map.py
+++ map.py
@@ -15,43 +15,44 @@ for line in f:
     location_names[internal_name] = name
 f.close()
 
+static_values = ET.parse('Static.xml').getroot().findall('./*/value/SaveFlag/..')
 
 botw_map = Image.open("BotW-Map.png")
 map_draw = ImageDraw.Draw(botw_map)
 
+f=open('tracked_locations.txt')
+f2=open('map_locations.js','w')
+f2.write('var locations = [\n')
+for line in f:
+    location = line.strip()
+    found = False
+    name = None
+    if location in location_names:
+        name = location_names[location]
+    for value in static_values:
+        saveflag = value.findall('./SaveFlag')[0].text
+        
+        if 'Location_' + location == saveflag:
+            messageIDs = value.findall('./MessageID')
+            if not name and len(messageIDs) and (messageIDs[0].text in location_names):
+                name = location_names[messageIDs[0].text]
+            if not name:
+                name = location
+            
+            translate = value.findall('./Translate')[0]
+            x,z = float(translate.attrib['X'].rstrip('f')), float(translate.attrib['Z'].rstrip('f'))
+            f2.write('{"internal_name":"%s", "display_name":"%s", "x":%g, "y":%g},\n' % (saveflag, name, x, z))
 
-root = ET.parse('Static.xml').getroot()
+            img_x = int(x/2 + 3000)
+            img_y = int(z/2 + 2500)
+            map_draw.ellipse((img_x-CIRCLE_RADIUS, img_y-CIRCLE_RADIUS, img_x+CIRCLE_RADIUS, img_y+CIRCLE_RADIUS), fill='cyan', outline='blue')
 
-for child in list(root[1]) + list(root[2]):
-    messageID = '{{NoName}}'
-    for subchild in child:
-        if subchild.tag == 'MessageID':
-            messageID = subchild.text
-        if subchild.tag == 'Translate':
-            x,_,z = float(subchild.attrib['X'].rstrip('f')), float(subchild.attrib['Y'].rstrip('f')), float(subchild.attrib['Z'].rstrip('f'))
-    if messageID == '{{NoName}}':
-        for subchild in child:
-            if subchild.tag == 'Icon':
-                messageID = subchild.text + '}'
-            if subchild.tag == 'SaveFlag':
-                messageID = '{' + subchild.text + ':' + messageID
-    if messageID in location_names:
-        name = location_names[messageID]
-    else:
-        name = messageID
+            found = True
+            break
+    if not found:
+        print('Not found %s (%s)' % (location, name))
 
-    if messageID.startswith('Dungeon'):
-        continue # ignore the 120 shrines
-
-    img_x = int(x)
-    img_y = int(z)
-    print('{"internal_name":"%s", "display_name":"%s", "x":%g, "y":%g},' % (messageID, name, x, z))
-
-    map_draw.ellipse((img_x-CIRCLE_RADIUS, img_y-CIRCLE_RADIUS, img_x+CIRCLE_RADIUS, img_y+CIRCLE_RADIUS), fill='cyan', outline='blue')
-
-    
-
+f2.write('];\n')
+f.close()
+f2.close()
 botw_map.save('BotW-Map-Labeled.png')
-
-# X ranges from -4870.5 to 4736.618, Z ranges from -3802.419 to 3840.943
-# LocationMarker len 215, LocationPointer len 131, total len 346 (120 shrines, 226 other)
